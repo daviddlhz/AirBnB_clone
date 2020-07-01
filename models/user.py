f06c9a9fcdcb56be84fa1d6867548bbc5e90db45
jesgogu27 <jesgogu@hotmail.com>
2020-06-30 23:56:57 -0500

    user

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""user module"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ 
+    	class User that inherits from BaseModel:
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""

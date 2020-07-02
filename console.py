96129f398bb29f46c6bab05d526ad4c0d8f17a15
daviddlhz <vidda1959@gmail.com>
2020-07-01 20:11:31 -0500

    Task 10 complete

--- console.py
+++ console.py
@@ -8,12 +8,21 @@ import shlex
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """command interpreter of AirBnB project
     """
-    classes = {"BaseModel": BaseModel, 'User': User}
+    classes = {'BaseModel': BaseModel, 'User': User,
+               'Place': Place, 'State': State,
+               'City': City, 'Amenity': Amenity,
+               'Review': Review}
+
     prompt = '(hbnb) '
 
     def do_EOF(self, line):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""
+FileStorage Class - serializes and deserializes JSON file to instances
+"""
 
 import json
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage():
@@ -12,14 +20,21 @@ class FileStorage():
     __objects = {}
 
     def all(self):
+        """
+        Method returns the dictionary __objects
+        """
         return (self.__objects)
 
     def new(self, obj):
+        """
+        sets in __objects the obj with class name and id
+        """
         if obj:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
+        """ serialization """
         aux = {}
         for key, value in self.__objects.items():
             aux[key] = value.to_dict()
@@ -27,7 +42,13 @@ class FileStorage():
             json.dump(aux, f)
 
     def reload(self):
-        cl = {'BaseModel': BaseModel, 'User': User}
+        """ deserialization """
+
+        cl = {'BaseModel': BaseModel,
+              'User': User, 'Place': Place,
+              'State': State, 'City': City,
+              'Amenity': Amenity, 'Review': Review}
+
         try:
             with open(self.__file_path, 'r') as filesave:
                 for key, value in json.load(filesave).items():
